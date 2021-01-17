48081bc5654d678b82e485e27fb7d4ffe2ea96e9
Tim Morgan <tmorgan99@gmail.com>
2021-01-16 17:09:04 -0800

    Timer and ProgressBar

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -1,3 +1,19 @@
-<h2>
-    Progresss Bar
-</h2>
+<div bp="grid">
+  <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+    <div class="progress-bar" style="width: 20%">
+      <span class="sr-only"> %</span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .progress-container {
+    margin: 20px 0;
+    border: solid gray 1px;
+  }
+
+  .progress-bar {
+    height: 30px;
+    background: green;
+  }
+</style>
--- src/components/Timer.svelte
+++ src/components/Timer.svelte
@@ -2,8 +2,23 @@
   import ProgressBar from "./ProgressBar.svelte";
 </script>
 
-<h2>
-  Seconds remain ..
-  <ProgressBar />
-  <button> Start </button>
-</h2>
+<div bp="grid">
+  <h2 bp="offset-5@md 4@md 12@sm">seconds remain</h2>
+</div>
+<ProgressBar />
+<div bp="grid">
+
+<button class="start" bp="offset-5@md 4@md 12@sm"> Start </button>
+</div>
+
+<style>
+  .start {
+    background-color: greenyellow;
+    width: 100%;
+    margin: 10px 0;
+
+  }
+  h2 {
+    margin: 0;
+  }
+</style>
